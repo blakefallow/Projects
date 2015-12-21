c1f17edeb5eb3a71e4c0f7648200e01603f80bd0
Joshua Stein <josh.stein@flexshopper.com>
2015-12-21 14:12:31 -0500

    update setup script with docker-osx-dev steps

--- setup.sh
+++ setup.sh
@@ -16,10 +16,17 @@ npm install
 bower install 
 gulp
 
+curl -o /usr/local/bin/docker-osx-dev https://raw.githubusercontent.com/brikis98/docker-osx-dev/master/src/docker-osx-dev
+chmod +x /usr/local/bin/docker-osx-dev
+
+cd ../flexbox
+docker-machine rm default
+docker-machine create --driver virtualbox --virtualbox-memory "4096" default
+eval "$(docker-machine env default)"
+docker-osx-dev install
+docker-osx-dev #This process must run in the background
 echo "\n\nBUILDING ENVIRONMNET\n\n"
 #How do I not run the next 2 lines if on Ubuntu
-docker-machine start default
-eval "$(docker-machine env default)"
 
-cd ../.. && docker-compose build && docker-compose up -d && docker-compose ps
+cd  && docker-compose build && docker-compose up -d && docker-compose ps
 
